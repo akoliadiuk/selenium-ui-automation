ca257d853ae83961d0dbf4291d7c1b74c5429207
akoli252 <39258964+akoli252@users.noreply.github.com>
2021-02-04 14:12:08 +0200

    Updated packages, babel config, webdriver config

--- test/webdriver.js
+++ test/webdriver.js
@@ -1,10 +1,20 @@
 require('chromedriver');
 export const webdriver = require("selenium-webdriver");
+const TIMEOUT = 30000;
+
 let chromeCapabilities = webdriver.Capabilities.chrome();
 let chromeOptions = { 'args': ['--start-maximized'] };
 chromeCapabilities.set('goog:chromeOptions', chromeOptions);
-export default new webdriver
+const browser = new webdriver
     .Builder()
     .forBrowser("chrome")
     .withCapabilities(chromeCapabilities)
     .build();
+
+    browser.then(async () => {
+        await browser
+            .manage()
+            .setTimeouts({ 'implicit': TIMEOUT, 'pageLoad': TIMEOUT, 'script': TIMEOUT })
+    });
+
+export default browser
